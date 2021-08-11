18b5a79ee24a0c9f7b00a0f9eccd6823ca770baf
Deise Santana <dsds3@aluno.ifnmg.edu.br>
2021-08-11 15:28:30 -0300

    Concluído com Responsivo

--- src/Pages/JokesGame/index.tsx
+++ src/Pages/JokesGame/index.tsx
@@ -2,36 +2,26 @@
 /* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
 import { CardContent, Typography } from '@material-ui/core';
-// import { withStyles } from '@material-ui/core/styles';
-import { Redirect } from 'react-router-dom';
-import { Joke } from '../../Types';
+import { Link } from 'react-router-dom';
 import { getJokeRandom } from '../../Api/Jokes';
 import * as T from './styles';
-
-// const WhiteTextTypography = withStyles({
-//   root: {
-//     color: '#FFFFFF',
-//     paddingLeft: '3%',
-//   },
-// })(Typography);
 
 const JokesGame = () => {
   const [word, setWord] = useState('');
   const [wordPlayer, setWordPlayer] = useState('');
   const [wordSplit, setWordSplit] = useState<string[]>([]);
-  const [click, setClick] = useState(false);
   const [qtd, setQtd] = useState(0);
   const [win, setWin] = useState(0);
-  const [text, setText] = useState('Calma');
+  const [click, setClick] = useState(false);
 
   const getJoke = async () => {
     try {
       const responseJoke = await getJokeRandom();
       const jokeSplit = responseJoke.data.value.joke.split(' ');
       const media = Math.trunc(jokeSplit.length / 2);
-      setWord(jokeSplit[media]);
+      setWord(jokeSplit[media].replace('&quot;', ''));
       console.log(jokeSplit[media]);
-      jokeSplit[media] = '?';
+      jokeSplit[media] = '????';
       setWordSplit(jokeSplit);
       setQtd(qtd + 1);
     } catch (error) {
@@ -39,27 +29,14 @@ const JokesGame = () => {
     }
   };
 
-  const videoWatch = () => {
-    if (text === 'Sucesso') {
-      // eslint-disable-next-line no-lone-blocks
-      {
-        <Redirect to="https://youtu.be/T24Cu94qsX4" />;
-      }
-    }
-  };
-
-  const winner = () => {
-    if (win === 2) {
-      setText('Segredo');
-    }
-  };
-
   const compWord = async () => {
-    if (word === wordPlayer) {
+    word.replace(/&quot;/g, '');
+    word.replace('.', '');
+
+    if (word.toLowerCase() === wordPlayer.toLowerCase()) {
       setClick(!click);
       setWordPlayer('');
       setWin(win + 1);
-      winner();
     } else {
       getJoke();
       setWordPlayer('');
@@ -68,50 +45,52 @@ const JokesGame = () => {
 
   useEffect(() => {
     getJoke();
-  }, [click, text]);
+  }, [click]);
 
   return (
     <T.ContainerMain>
       <T.Body>
         <T.ContainerForm>
-          <T.Form>
-            <T.CardJoke>
-              <CardContent>
-                <Typography variant="h5">{wordSplit.map((j) => `${j} `)}</Typography>
-              </CardContent>
-            </T.CardJoke>
-            <T.CardJokePlayer>
-              <div>
-                <T.TextFieldMod
-                  required
-                  id="standard-required"
-                  variant="outlined"
-                  defaultValue=""
-                  onChange={(e) => setWordPlayer(e.target.value)}
-                />
-              </div>
-              <T.Ajuda>
-                <T.ButtonWin variant="contained" color="default" onClick={() => compWord()}>
-                  Press
-                </T.ButtonWin>
-              </T.Ajuda>
-            </T.CardJokePlayer>
-          </T.Form>
+          <T.CardJoke>
+            <CardContent>
+              <Typography variant="h5">{wordSplit.map((j) => `${j.replace(/&quot;/g, '"')} `)}</Typography>
+            </CardContent>
+          </T.CardJoke>
+          <T.CardJokePlayer>
+            <T.Joke>
+              <T.TextFieldMod
+                required
+                id="standard-required"
+                variant="outlined"
+                defaultValue=""
+                onChange={(e) => setWordPlayer(e.target.value)}
+              />
+            </T.Joke>
+            <T.ButtonJoke>
+              <T.ButtonWin variant="contained" color="default" onClick={() => compWord()}>
+                Answer
+              </T.ButtonWin>
+            </T.ButtonJoke>
+          </T.CardJokePlayer>
         </T.ContainerForm>
-        <T.ContainerScore>
-          Pergunta: {qtd} <p />
-          Acertadas: {win} <p />
-          <T.ButtonWin variant="contained" color="default" onClick={() => videoWatch()}>
-            {text}
-          </T.ButtonWin>
-        </T.ContainerScore>
       </T.Body>
+      <T.ContainerScore>
+        Question: {qtd} <p />
+        Hits: {win} <p />
+      </T.ContainerScore>
       <T.Process>
-        <Typography variant="h5">
+        <Typography variant="h6">
           Está sendo processado por essa risada! <p />
           Ass: Eu, Chuck Norris!
         </Typography>
       </T.Process>
+      <T.ContainerAnswer>
+        Nós meros mortais precisamos saber sobre o sentido da vida, em algum ponto desse site está a{' '}
+        <T.InputResposta href="https://youtu.be/T24Cu94qsX4">resposta</T.InputResposta>.
+      </T.ContainerAnswer>
+      <T.Footer>
+        <Link to="https://youtu.be/T24Cu94qsX4" />
+      </T.Footer>
     </T.ContainerMain>
   );
 };
